793f8b37c2b0cdb1a1de0a838b4eb06d7b2b9c0a
bj7hg <bj57hg@gmail.com>
2022-05-19 23:49:29 +0300

    Layout

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,10 +1,17 @@
 const burger = document.querySelector('.burger-menu');
 const burgerMenu = document.querySelector('.menu-wrapper');
 const closeBurger = document.querySelector('.close-button-burger');
-const menu = document.getElementsByClassName('.menu-item');
+const menu = document.querySelectorAll('.menu-item');
+console.log(menu);
 burger.addEventListener("click", function(e) {
     burgerMenu.classList.add('active');
 });
 closeBurger.addEventListener("click", function(e) {
     burgerMenu.classList.remove('active');
 });
+// menu.addEventListener("click", function(e) {
+//     this.classList.add('active');
+// });
+menu[0].onclick = function() {
+    this.classList.add('active');
+}
